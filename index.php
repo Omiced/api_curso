b095014586bf16faad1cec641bd66193a613ee0c
Josue Cano <jdevelopcano@gmail.com>
2024-03-07 22:22:31 -0600

    Added taskcontroller

--- index.php
+++ index.php
@@ -4,6 +4,19 @@ $parts = explode('/', $path);
 
 $resource = $parts[2];
 $id = $parts[3] ?? null;
+$method =  $_SERVER['REQUEST_METHOD'];
+// http status code para las url no permitidas
+if($resource !== "tasks"){
+  //obtenemos el protocolo http usado y usando el metodo header pasamos el status code
+  // header("{$_SERVER['SERVER_PROTOCOL']} 404 not found");
+  //usando http_response este es el metodo recomendado
+  http_response_code(404);
+  exit;
+}
 
-echo $resource, ' ', $id,'';
-echo $_SERVER['REQUEST_METHOD'];
+
+require dirname(__DIR__) . "/api_curso/src/TaskController.php";
+
+$controller = new TaskController;
+
+$controller->processRequest($method, $id);
